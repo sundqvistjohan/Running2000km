d6a437edb2e4e093c6bcf928f505af114b66a9be
Johan Sundqvist <sundqvist.johan@outlook.com>
2019-11-19 09:29:13 +0100

    Changes runDistanceCalc to function instead of class, some refactoring

--- runDistanceCalc.js
+++ runDistanceCalc.js
@@ -1,21 +1,24 @@
-class runDistanceCalc {
-    constructor(startDistance, goalDistance) {
-        this.startDistance = startDistance;
-        this.goalDistance = goalDistance;
+var runDistanceCalc = function () {
+    function runDistanceCalc(startDistance, goalDistance) {
+      this.startDistance = startDistance;
+      this.goalDistance = goalDistance;
     }
-
-    avgDistanceCalc() {
-       const daysLeftInYear = this.dateCalc();
-       let avgDistance = (this.goalDistance - this.startDistance) / daysLeftInYear;
-       return avgDistance;
-    }
-    
-    dateCalc() {
-        const currentDate = new Date;
-        const nextYear = currentDate.getFullYear() + 1;
-        const finalDate = new Date(nextYear, 0, 1);
-        const oneDay = 24 * 60 * 60 * 1000;
-        const diffDays = Math.round(Math.abs((finalDate-currentDate) / oneDay));
-        return diffDays;
-    }
-}
+  
+    runDistanceCalc.prototype.avgDistanceCalc = function avgDistanceCalc() {
+      const daysLeftInYear = this.daysLeftCalc();
+      const avgDistance = (this.goalDistance - this.startDistance) / daysLeftInYear;
+      return avgDistance;
+    };
+  
+    runDistanceCalc.prototype.daysLeftCalc = function daysLeftCalc() {
+      const currentDate = new Date();
+      const nextYear = currentDate.getFullYear() + 1;
+      const finalDate = new Date(nextYear, 0, 1);
+      const oneDay = 24 * 60 * 60 * 1000;
+      const daysLeft = Math.round(Math.abs((finalDate - currentDate) / oneDay));
+      return daysLeft;
+    };
+  
+    return runDistanceCalc;
+  }();
+ 
